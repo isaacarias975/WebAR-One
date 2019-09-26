a7e9fa872e49608dd700f4580d9c6a7aeb4c0dbc
isaacarias975 <48956335+isaacarias975@users.noreply.github.com>
2019-09-26 14:40:37 -0500

    Desplegando WebAR

---
+++ precache-manifest.fa7ebdf439277e3515366468c10b8f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4414e6af2fc6832220e09449bcea46d",
+    "url": "/WebAR-One/index.html"
+  },
+  {
+    "revision": "0515da98642ec134688a",
+    "url": "/WebAR-One/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3aa86cdd722894af1add",
+    "url": "/WebAR-One/static/js/2.5ed1cf26.chunk.js"
+  },
+  {
+    "revision": "0515da98642ec134688a",
+    "url": "/WebAR-One/static/js/main.04189645.chunk.js"
+  },
+  {
+    "revision": "51d63a9296049ccff305",
+    "url": "/WebAR-One/static/js/runtime-main.220898e0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/WebAR-One/static/media/logo.25bf045c.svg"
+  }
+]);
